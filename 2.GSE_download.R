99e6f53f7ddc4ff3e629243d79b24d151470c870
Matteo Ramazzotti <matteo.ramazzotti@unifi.it>
2024-03-03 01:31:59 +0100

    Update 2.GSE_download.R

--- 2.GSE_download.R
+++ 2.GSE_download.R
@@ -3,6 +3,7 @@
 
 #delete existing log files
 system(("unlink seq_exp_log")
+
 source("RNAseq_autodown.R")
 seq_exp=read.delim("GSE_seq")[,1]
 seq_dataset=NULL
@@ -18,12 +19,13 @@ for (i in 1:length(seq_exp)) {
 	}
 }
 
-#the seq_exp_log file is filtered to select rows (experiments) matching the cell MCF7 
+#the seq_exp_log file is filtered to select rows (experiments) matching the cell name MCF7 
 system("sort seq_exp_log | uniq | grep -i MCF7 | perl -ne 's/^ //;print' > seq_exp_log_MCF7")
-#this strips white specaes at the beginning of the line
-#system("perl -ne 's/^ //;print' seq_exp_log_MCF7 > seq_exp_log_MCF7")
 
-#manual selection of experiments based on the "seq_exp_log" file
+#this strips white specaes at the beginning of the line
+system("perl -ne 's/^ //;print' seq_exp_log_MCF7 > seq_exp_log_MCF7")
+
+#manual selection of experiments based on the "seq_exp_log_MCF7" file
 #the aim is to create a two column data frame with
-#GSExxxx	sample_name
+#GSExxxx    sample_name
 #named seq_exp_log_MCF7_ok
