50626c500eaca74148ac67eeba7ed0ff8008e3e0
Von B. <lorenzo.casbarra@gmail.com>
2025-10-14 23:32:15 +0200

    feat: uploaded 5.de.R

---
+++ 5.de.R
@@ -0,0 +1,22 @@
+library(DESeq2)
+
+counts <- read.delim("selected_data.tsv",sep="\t",row.names = 1)
+metadata <- read.delim("selected_metadata.tsv",sep="\t")
+
+counts <- counts[rowSums(counts) > 10, ] #remove low count genes before normalization
+
+dds <- DESeqDataSetFromMatrix(
+	countData = counts,
+	colData = metadata,
+	design = ~GROUP
+)
+dds <- DESeq(
+	dds
+)
+res <- results(dds, contrast=c("GROUP","G1","G2"))
+#res <- lfcShrink(dds, coef=paste(sep="_","GROUP","G2","vs","G1"), type="apeglm")
+
+
+res_filtered <- res[res$padj < 0.05 & abs(res$log2FoldChange) > 1.5 ,]
+
+write.table(res_filtered, file = "de_data.tsv", sep = "\t")
